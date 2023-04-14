75ebb999a2bb053078f3e0b57c48623599584fc1
jihunhong <redgee49@gmail.com>
2023-04-14 21:27:17 +0900

    tesseract try failed

---
+++ README.md
@@ -0,0 +1,5 @@
+# marvelsnap-card-ocr project
+
+- tesseract 시도
+  - 폰트가 너무 다양해서 실패
+  - jTessBoxEditor 노가다성이 너무 심함
--- test.js
+++ test.js
@@ -2,11 +2,13 @@ const path = require("path");
 const Tesseract = require("tesseract.js");
 const { createWorker } = Tesseract;
 (async () => {
-  const worker = await createWorker();
+  const worker = await createWorker({
+    logger: (m) => console.log(m),
+  });
   await worker.loadLanguage("eng");
   await worker.initialize("eng");
   const {
     data: { text },
-  } = await worker.recognize(path.resolve(__dirname, "./images", "basic.png"));
+  } = await worker.recognize(path.resolve(__dirname, "./images", "eng_1.jpeg"));
   console.log(text);
 })();
